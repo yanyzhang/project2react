735879467c73944ed0009777828f0efe6ca5fb8e
Yan Zhang <yanyzhang1@gmail.com>
2024-06-04 00:20:57 -0400

    Configure React app for deployment to GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-const userName = "Peter Smith";
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div>
-    <h2>Main Title</h2>
-    <h3>Hello Again {userName} </h3>
-    <p>some text in a paragraph</p>
-  </div>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
