ddc03aa39f4e428534098d3a199a9a8532626943
Maria <weetziebat92@gmail.com>
2024-06-07 12:33:06 -0400

    adding card layout

--- src/about.jsx
+++ src/about.jsx
@@ -4,18 +4,25 @@ function About() {
   return (
     <div
       style={{
-        display: "flex",
+        // display: "flex",
         justifyContent: "center",
         alignItems: "center",
         height: "100%",
       }}
     >
-      <h1>
-        This is a woman owned solar panel company. Lorem ipsum dolor sit amet
+      <h1>Hello</h1>
+      <p>
+        This is a woman owned solar company. Lorem ipsum dolor sit amet
         consectetur adipisicing elit. Aspernatur iste sunt sint nemo deserunt
         animi deleniti corporis, voluptatem commodi placeat eius. A magni
-        voluptates similique incidunt veritatis possimus sapiente iste.
-      </h1>
+        voluptates similique incidunt veritatis possimus sapiente iste. Lorem
+        ipsum dolor sit amet consectetur, adipisicing elit. Magnam veritatis cumque, 
+        voluptatibus impedit saepe quas deleniti facere atque magni ducimus doloribus 
+        eveniet alias et architecto placeat enim, officiis animi omnis? <br/><br/>
+
+        Images are from this <a href="https://signaturesolar.com/all-products/kits-bundles/">solar company</a> and are being used for classwork purposes!
+      </p>
+      
     </div>
   );
 }
